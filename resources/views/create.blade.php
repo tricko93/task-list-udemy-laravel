1a2a56ef9f1955bf41076006c498f0b03d79db75
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2023-11-19 17:38:19 +0100

    refactor Extract form.blade.php from views
    - Create a new file form.blade.php that contains the form structure for
      adding and editing tasks. This will avoid duplication and improve
      maintainability.
    
    - Update create.blade.php and edit.blade.php to include the
      form.blade.php file. This will keep the views simple and consistent.

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,48 +5,6 @@
 
 @extends('layouts.app')
 
-@section('title', 'Add Task')
-
-@section('styles')
-	<style>
-		.error-message {
-			color: red;
-			font-size: 0,8rem;
-		}
-	</style>
-@endsection
-
 @section('content')
-	<form method="POST" action="{{ route('tasks.store') }}">
-		@csrf
-		<div>
-			<label for="title">
-				Title
-			</label>
-			<input text="text" name="title" id="title" />
-			@error('title')
-				<p class="error-message">{{ $message }}</p>
-			@enderror
-		</div>
-
-		<div>
-			<label for="description">Description</label>
-			<textarea name="description" id="description" rows="5"></textarea>
-			@error('description')
-				<p class="error-message">{{ $message }}</p>
-			@enderror
-		</div>
-
-		<div>
-			<label for="long_description">Description</label>
-			<textarea name="long_description" id="long_description" rows="10"></textarea>
-			@error('long_description')
-				<p class="error-message">{{ $message }}</p>
-			@enderror
-		</div>
-
-		<div>
-			<button type="submit">Add Task</button>
-		</div>
-	</form>
+	@include('form')
 @endsection
